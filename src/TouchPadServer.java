ca83bad7ddb49f45f865dfa3336e910182147530
Dylange <dylan_lange@live.com>
2016-10-06 03:22:36 +1300

    implemented clicking

--- src/TouchPadServer.java
+++ src/TouchPadServer.java
@@ -1,15 +1,17 @@
+import java.awt.AWTException;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 public class TouchPadServer {
 
 	private static final int PORT = 6969;
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException, AWTException{
 		Robot robot = new Robot();
 		
 		DatagramSocket serverSocket = new DatagramSocket(PORT);
@@ -25,21 +27,34 @@ public class TouchPadServer {
 			String sentence = new String( receivePacket.getData());
 			System.out.println("RECEIVED: " + sentence);
 			
-			float dx = Float.parseFloat(sentence.split(":")[0]);
-			float dy = Float.parseFloat(sentence.split(":")[1]);
-			
-			Point mouseCoord = MouseInfo.getPointerInfo().getLocation();
-			robot.mouseMove((int)(mouseCoord.getX() + dx), (int)(mouseCoord.getY() + dy));
+			if(sentence.startsWith("MOVE")){
+				float dx = 0;
+				float dy = 0;
+				
+				try{
+					dx = Float.parseFloat(sentence.split("MOVE")[1].split(":")[0]);
+					dy= Float.parseFloat(sentence.split("MOVE")[1].split(":")[1]);
+				} catch (NumberFormatException e){
+					e.printStackTrace();
+					continue;
+				}
+				
+				Point mouseCoord = MouseInfo.getPointerInfo().getLocation();
+				robot.mouseMove((int)(mouseCoord.getX() + dx), (int)(mouseCoord.getY() + dy));
+			} else if(sentence.startsWith("CLICK")){
+				robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+				robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			}
 			//System.out.println("X: " + dx + " | Y:" + dy);
 			
-			InetAddress IPAddress = receivePacket.getAddress();
+			/*InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			
 			String capitalizedSentence = sentence.toUpperCase();
 			sendData = capitalizedSentence.getBytes();
 			
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
+			serverSocket.send(sendPacket);*/
 		}
 		
 	}
